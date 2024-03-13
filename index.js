0dc00e95d851d4fbc252501126dfd5204ebbdbc6
William Ndirangu <wwagura0@gmail.com>
2024-03-13 17:43:42 +0300

    Adds function to code

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+    return string.toUpperCase();
+}
+function whisper(string){
+    return string.toLowerCase()
+}
+function logShout(string){
+    console.log(string.toUpperCase());
+}
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+    if (typeof string === 'string') {
+      if (string === string.toLowerCase()) {
+        console.log("I can't hear you!");
+        return "I can't hear you!";
+      } 
+      else if (string === string.toUpperCase()) {
+        console.log('YES INDEED!');
+        return 'YES INDEED!';
+      }
+       else {
+        console.log('I would love to!');
+        return 'I would love to!';
+      }
+    }
+  }
